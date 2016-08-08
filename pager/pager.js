0427db725171909295580c79ddd7b68e2e39f1d8
林蛤蛤 <linzb93@qq.com>
2016-08-08 22:21:43 +0800

    update

--- pager/pager.js
+++ pager/pager.js
@@ -1,13 +1,15 @@
 ;(function($) {
     var d = {
         cell: 'li',
-        count: 10
+        count: 10,
+        page: '.page'
     };
     function Pager($this, option){
         this.$this = $this;
         var o = $.extend({}, d, option);
         this.o = o;
-        this.cell = this.$this.find(this.o.cell);
+        this.perCell = this.$this.find(this.o.cell);
+        this.page = $(this.o.page);
     }
     function countPageNum(ele, count) {
         return Math.floor(ele.length / count) + 1;
@@ -20,9 +22,41 @@
                 this.cell.eq(i).show();
             }
         },
-        createPager: function() {},
-        changePage: function() {
-            this.$this.find('a').on('click', function() {});
+        createPager: function() {
+            var pageHtml = '';
+            pageHtml += '<a href="javascript:;">首页</a>';
+            pageHtml += '<a href="javascript:;">上一页</a>';
+            for(var i = 0; i < 5; i++) {
+                pageHtml += '<a href="javascript:;">' + i + '</a>';
+            }
+            pageHtml += '<a href="javascript:;">下一页</a>';
+            pageHtml += '<a href="javascript:;">尾页</a>';
+            this.page.append(pageHtml);
+        },
+        getCurPage: function() {
+            this.$this.find('a').on('click', function() {
+                var $text = $(this).text(),
+                curNum = 0;
+                if (parseInt($text) > 0) {
+                    curNum = parseInt($text);
+                } else {
+                    switch () {
+                        case '首页':
+                            curNum = 1;
+                            break;
+                    }   case '上一页':
+                            break;
+                        case '下一页':
+                            break;
+                        case '尾页':
+                            curNum =
+                            break;
+                }
+            });
+        },
+        changePage: function(num) {
+            //执行切换页面
+            //分页器变换
         }
     })
     $.fn.pager = function(option) {
