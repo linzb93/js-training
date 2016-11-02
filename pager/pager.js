204f8c3895e8a7b08a71f118c02c594064ac4fea
linzhibin <linzb93@qq.com>
2016-11-02 21:57:21 +0800

    update

--- pager/pager.js
+++ pager/pager.js
@@ -1,16 +1,16 @@
-;(function($) {
+(function($) {
     var d = {
         cell: 'li',
         count: 10,
         pagination: '',
     };
+
     function Pager($this, option){
         this.$this = $this;
-        var o = $.extend({}, d, option);
-        this.o = o;
-        this.perCell = this.$this.find(this.o.cell);
-        this.pagination = $(this.o.pagination);
-        this.totalNum = countTotalPage(this.perCell, this.o.count);
+        this.o = $.extend({}, d, option);
+        this.$perCell = this.$this.find(this.o.cell);
+        this.$pagination = $(this.o.pagination);
+        this.totalNum = countTotalPage(this.$perCell, this.o.count);
         this.curNum = 1;
         this.prevNum = 1;
         var $firstLink, $prevLink, $nextLink, $lastLink, $pagiNumLink;
@@ -18,44 +18,69 @@
         this.getCurPage();
     }
     function countTotalPage(ele, count) {
-        return Math.floor(ele.length / count) + 1;
+        return Math.ceil(ele.length / count) + 1;
     }
     function canHidePrev(cur) {
         return cur < 3;
     }
     function canHideNext(cur, total) {
-        return total - cur < 3;
+        return total - cur < 4;
     }
+
     $.extend(Pager.prototype, {
         init: function() {
-            this.perCell.hide();
+            this.$perCell.hide();
             for (var i = 0; i < this.o.count; i++) {
-                this.perCell.eq(i).show();
+                this.$perCell.eq(i).show();
             }
-            this.createPager();
+            this.createPager(0);
         },
-        createPager: function() {
+        createPager: function(curNum) {
             var pageHtml = '';
-            pageHtml += '<a class="pager-pagi-first" href="#">首页</a>';
-            pageHtml += '<a class="pager-pagi-prev" href="#">上一页</a>';
-            for (var i = 1; i <= 5; i++) {
-                pageHtml += '<a class="pager-pagi-num" href="#">' + i + '</a>';
+            var i, j = 0;
+            var tot = Math.min(this.totalNum, 5);
+            if (canHidePrev(curNum)) {
+                i = 1;
+            } else if (canHideNext(curNum, this.totalNum)) {
+                i = this.totalNum - 5;
+            } else {
+                i = curNum - 1;
             }
-            pageHtml += '<a class="pager-pagi-next" href="#">下一页</a>';
-            pageHtml += '<a class="pager-pagi-last" href="#">尾页</a>';
-            this.pagination.append(pageHtml);
+            pageHtml += '<a class="pager-pagi-first pager-btn" href="#">首页</a>';
+            pageHtml += '<a class="pager-pagi-prev pager-btn" href="#">上一页</a>';
+            for (; j < tot - 1; i++) {
+                pageHtml += '<a class="pager-pagi-num" href="#">' + i + '</a>';
+                j++;
+            }
+            pageHtml += '<a class="pager-pagi-next pager-btn" href="#">下一页</a>';
+            pageHtml += '<a class="pager-pagi-last pager-btn" href="#">尾页</a>';
+            this.$pagination.append(pageHtml);
             $firstLink = $('.pager-pagi-first');
             $prevLink = $('.pager-pagi-prev');
             $nextLink = $('.pager-pagi-next');
             $lastLink = $('.pager-pagi-last');
             $pagiNumLink = $('.pager-pagi-num');
-            $pagiNumLink.first().addClass('on');
-            $firstLink.hide();
-            $prevLink.hide();
+            if (curNum === 0) {
+                $pagiNumLink.eq(0).addClass('on');
+            } else {
+                $pagiNumLink.each(function(i) {
+                if ($(this).text() == curNum) {
+                    $pagiNumLink.eq(i + 1).addClass('on');
+                }
+                return;
+            })
+            }
+            if (canHidePrev(curNum)) {
+                $firstLink.hide();
+                $prevLink.hide();
+            } else if (canHideNext(curNum, this.totalNum)) {
+                $nextLink.hide();
+                $lastLink.hide();
+            }
         },
         getCurPage: function() {
             var that = this;
-            this.pagination.find('a').on('click', function() {
+            this.$pagination.on('click', 'a', function() {
                 var $text = $(this).text();
                 if (parseInt($text) > 0) {
                     that.curNum = parseInt($text);
@@ -71,7 +96,7 @@
                         that.curNum++;
                         break;
                         case '尾页':
-                        that.curNum = that.totalNum - 1;
+                        that.curNum = that.totalNum;
                         break;
                     }
                 }
@@ -82,45 +107,16 @@
             //执行切换页面
             var that = this;
             for (var i = (this.prevNum - 1) * this.o.count; i < this.prevNum * this.o.count; i++) {
-                this.perCell.eq(i).hide();
+                this.$perCell.eq(i).hide();
             }
             for (var i = (this.curNum - 1) * this.o.count; i < this.curNum * this.o.count; i++) {
-                this.perCell.eq(i).show();
+                this.$perCell.eq(i).show();
             }
             this.prevNum = this.curNum;
+
             //分页器变换
-            if (canHidePrev(this.curNum)) {
-                $firstLink.hide();
-                $prevLink.hide();
-                $nextLink.show();
-                $lastLink.show();
-            } else if (canHideNext(this.curNum, this.totalNum)) {
-                $firstLink.show();
-                $prevLink.show();
-                $nextLink.hide();
-                $lastLink.hide();
-            } else {
-                this.pagination.find('a').show();
-            }
-            var addNum = 0;
-                if (this.curNum == 1) {
-                    addNum = 1;
-                } else if (this.curNum == this.totalNum - 1) {
-                    addNum = this.totalNum - 4;
-                } else {
-                    addNum = this.curNum - 2;
-                }
-                this.pagination.find('a').each(function(index, el) {
-                    if ($(this).hasClass('pager-pagi-num')) {
-                        $(this).text(addNum);
-                        addNum++;
-                    }
-                });
-            $pagiNumLink.each(function() {
-                    if ($(this).text() == that.curNum) {
-                        $(this).addClass('on').siblings().removeClass('on');
-                    }
-                });
+            this.$pagination.empty();
+            this.createPager(this.curNum - 1);
         }
     })
     $.fn.pager = function(option) {
