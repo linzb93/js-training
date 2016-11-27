e722dac512b3f4eb3ced1ecb0a901d48542a26e8
林蛤蛤 <linzb93@qq.com>
2016-11-27 18:49:38 +0800

    update

--- accordion/accordion.js
+++ accordion/accordion.js
@@ -1,20 +1,28 @@
+/*
+ * accordion.js
+ * https://github.com/linzb93/js-training/tree/master/accordion
+ * @license MIT licensed
+ *
+ * Copyright (C) 2016 linzb93
+ */
 (function($) {
+    //默认变量
     var d = {
         activeSize: 0,
         speed: 500,
         dir: 'h',
         css3Support: false,
+        easing: 'swing',
         beforeAnimation: function() {},
         afterAnimation: function() {}
     };
 
-    var acc = null;
-
     function Accordion($this, option) {
         this.$this = $this;
-        this.o = $.extend({}, d, option);
-        this.$list = this.$this.children();
-        this.w = this.$list.width();
+        this.o = $.extend({}, d, option);  //合并后的option
+        this.$child = this.$this.children();
+        this.speed = this.o.speed;
+        this.size = this.o.dir === 'h' ? this.$child.width() : this.$child.height();  //正常状态下的元素尺寸
         this.curIndex = 0;
         this.prevIndex = 0;
 
@@ -22,17 +30,22 @@
     }
 
     Accordion.prototype = {
+        //样式初始化
         init: function() {
             if (this.o.css3Support) {
-                this.$list.first().addClass('on');
+                this.$child.first().addClass('on');
             } else {
-                this.$list.first().width(this.o.activeSize);
+                this.o.dir === 'h' ?
+                this.$child.first().width(this.o.activeSize) :
+                this.$child.first.height(this.o.activeSize);
             }
             this.initEvent();
         },
+
+        //绑定事件
         initEvent: function() {
             var that = this;
-            this.$list.on('mouseenter', function() {
+            this.$child.on('mouseenter', function() {
                 var index = $(this).index();
                 if (index !== that.curIndex) {
                     that.prevIndex = that.curIndex;
@@ -41,36 +54,53 @@
                 }
             })
         },
+
+        doBeforeAnimation: function(num) {
+            if (this.o.beforeAnimation) {
+                this.o.beforeAnimation(num, this.$child.eq(num), this.prevIndex, this.$child.eq(this.prevIndex));
+            }
+        },
+
+        //执行动画
         accAnimation: function(num) {
-            this.o.beforeAnimation();
             var that = this;
+            this.doBeforeAnimation(num);
             if (this.o.css3Support) {
-                this.$list.eq(num).addClass('on').siblings().removeClass('on');
                 setTimeout(function() {
-                    that.o.afterAnimation();
-                }, that.o.speed + 20);
+                    that.doAfterAnimation(num);
+                }, this.speed + 10);
             } else {
-                this.$list.eq(num).stop(true).animate({
-                    width: this.o.activeSize
-                }, this.o.speed, 'linear');
-                this.$list.eq(this.prevIndex).stop(true).animate({
-                    width: this.w
-                }, this.o.speed, 'linear', function() {
-                    that.o.afterAnimation();
-                });
+                if (this.o.dir === 'h') {
+                    this.$child.eq(num).stop(true).animate({
+                        width: this.o.activeSize
+                    }, this.speed, this.o.easing);
+                    this.$child.eq(this.prevIndex).stop(true).animate({
+                        width: this.size
+                    }, this.speed, this.o.easing, function() {
+                        that.doAfterAnimation();
+                    });
+                } else {
+                    this.$child.eq(num).stop(true).animate({
+                        height: this.o.activeSize
+                    }, this.speed, this.o.easing);
+                    this.$child.eq(this.prevIndex).stop(true).animate({
+                        height: this.w
+                    }, this.speed, this.o.easing, function() {
+                        that.doAfterAnimation();
+                    });
+                }
+            }
+        },
+
+        doAfterAnimation: function(num) {
+            if (this.o.afterAnimation) {
+                this.o.afterAnimation(num, this.$child.eq(num), this.prevIndex, this.$child.eq(this.prevIndex));
             }
         }
     };
 
     $.fn.accordion = function(option) {
-        acc = new Accordion($(this), option);
+        new Accordion(this, option);
         return this;
-    }
-
-    //暴露方法
-    $.each(['getIndex', 'getElement'], function(key, val) {
-        $.fn.accordion[val] = function() {
-            return acc[val].apply(acc, arguments);
-        }
-    })
+    };
 })(jQuery);
