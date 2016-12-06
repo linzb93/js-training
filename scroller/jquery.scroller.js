76c19e03c210d52f68346fb96f0fb016710b28b5
林蛤蛤 <linzb93@qq.com>
2016-12-06 23:00:44 +0800

    添加轮播插件

--- scroller/jquery.scroller.js
+++ scroller/jquery.scroller.js
@@ -1,90 +1,140 @@
-(function($){
-    var d = {
-        scroollBar: '',  //滚动区域
-        scrollSlider: '',  //滚动条
-        speed: 500,  //滑动速度
-        fullScreen: false, //是否是全屏
-        perScrollGap: 50,  //每次滚轮滚动的距离
-        resizeOnChange: false,  //窗口缩放时是否重置
-        resetAfterLoad: false,  //图片加载完成后是否重置
+(function($) {
+    "use strict";
+
+    var pluginName = 'scroller';
+
+    //参数默认值
+    var defaults = {
+        dir: 'v',
+        scrollGap: 50,
+        speed: 300,
+        ns: pluginName,
+        mousewheel: false,
+        key: false
     };
 
-    function Scroll($this, option) {
+    //keyCode
+    var LEFT_KEY = 37;
+    var UP_KEY = 38;
+    var RIGHT_KEY = 39;
+    var DOWN_KEY = 40;
+
+    function Scroller($this, o) {
         this.$this = $this;
-        this.o = $.extend({}, d, option);
-        this.scroollBar = $(this.o.scroollBar);
-        this.scrollSlider = $(this.o.scrollSlider);
+        this.o = $.extend({}, defaults, o);
+
+        this.init();
     }
 
-    //TODO:懒加载下的滚动条重载（不是回到顶部）
-
-    $.extend(Scroller.prototype, {
+    Scroller.prototype = {
         init: function() {
-            var that = this,
-                tempHtml = '<div class="scroller-cont-wrapper"></div>',
-                scrollCont = this.$this.html();
-            this.$this.html(tempHtml);
-            this.wrapper = this.$this.children('scroller-cont-wrapper');
-            this.wrapper.html(tempHtml);
-            if (this.o.fullScreen) {
-                this.$this.innerHeight($(window).height());
+            this.$this.addClass(this.o.ns + '-container');
+            this.$this.children().wrapAll('<div class="' + this.o.ns + '-wrapper"></div>');
+
+            this.$scrollerWrapper = $('.' + this.o.ns + '-wrapper');
+
+            this.createBar();
+
+        },
+        createBar: function() {
+            var scrollHtml = '\
+            <div class="' + this.o.ns + '-bar">\
+            <div class="' + this.o.ns + '-block">\
+            </div>\
+            </div>';
+            this.$this.append(scrollHtml);
+
+            this.$scrollerBar = $('.' + this.o.ns + '-bar');
+            this.$scrollerBlock = $('.' + this.o.ns + '-block');
+            if (this.o.dir === 'v') {
+                this.maxMove = this.$scrollerBar.height() - this.$scrollerBlock.height();
+                this.rate = (this.$scrollerWrapper.height() - this.$this.height()) / this.maxMove + 0.1;
+            } else {
+                this.maxMove = this.$scrollerBar.width() - this.$scrollerBlock.width();
+                this.rate = (this.$scrollerWrapper.width() - this.$this.width()) / this.maxMove + 0.1;
             }
-            this.rate = Math.floor(this.scroollBar.innerHeight() / this.wrapper.innerHeight());
-            if (this.rate <= 1) {
-                this.scroollBar.hide();
-            }
-            if (this.o.resetAfterLoad) {
-                $('img').on('load', function() {
-                    that.init();
+
+
+            this.eventsHandler();
+        },
+        eventsHandler: function() {
+            this.dragEvent();
+            this.mousewheelEvent();
+            this.keyboardEvent();
+        },
+        reset: function() {},
+        dragEvent: function() {
+            var that = this;
+
+            this.$scrollerBlock.on('mousedown', function(e) {
+                var $this = $(this);
+                var gap = that.getBlockCurPos(e, $(this));
+
+                e.preventDefault();
+                $(this).on('selectstart', function() {
+                    return false;
+                });
+                $('body').on('mousemove.scroller', function(e) {
+                    var curBlockPos = (that.o.dir === 'v' ? e.pageY : e.pageX) - gap;
+                    curBlockPos = that.changeBlockPos(curBlockPos, that.maxMove);
+                    that.doScrollAnimation($this, curBlockPos);
+                    that.doScrollAnimation(that.$scrollerWrapper, -that.rate * curBlockPos);
+                });
+                $('body').on('mouseup', function() {
+                    $(this).off('mousemove.scroller');
+                    $this.off('selectstart');
+                });
+            })
+        },
+        mousewheelEvent: function() {
+
+            if (this.o.mousewheel) {
+                var that = this;
+                var curBlockPos;
+                this.$this.on('mousewheel DOMMouseScroll', function(e) {
+                    if (e.originalEvent.wheelDelta || e.originalEvent.detail) {
+                        curBlockPos = that.getBlockCurPos(e, $(this)) + that.o.scrollGap / that.rate;
+
+                    } else {
+                        curBlockPos = that.getBlockCurPos(e, $(this)) - that.o.scrollGap;
+                    }
+                    that.doScrollAnimation(that.$scrollerBlock, curBlockPos, true);
+                    that.doScrollAnimation(that.$scrollerWrapper, -that.rate * curBlockPos, true);
                 });
             }
         },
+        keyboardEvent: function() {
 
-        initEvent: function() {
-            //滑块拖拽
-            this.scrollSlider.on('mousedown', function() {
-                $(this).on('mousemove', function() {});
-            });
-            //鼠标滚轮滚动
-            $(document).on('mousewheel DOMMouseScroll', function(e) {
-                e.preventDefault();
-                (e.originalEvent.wheelDelta || -e.originalEvent.detail) > 0 ?
-            });
         },
-        scrollTo: function() {
-            var that = this;
-            this.wrapper.stop().animate({top:}, that.o.speed);
+        getBlockCurPos: function(e, $ele) {
+            return this.o.dir === 'v' ?
+            e.pageY - $ele.offset().top :
+            e.pageX - $ele.offset().left;
         },
-        cal: function() {}
-    });
-
-    $.fn.scroller = function(option) {
-        new Scroller($(this), option);
-    };
-}(jQuery));
-
-//滚动条
-(function($) {
-    var $bar = $('.scroll-bar');
-    var $block = $('.scroll-block');
-    var maxMove = $bar.height() - $block.height();
-    $block.on('mousedown', function(e) {
-        var pageTop = e.pageY;
-        $(this).on('mousemove', function(e) {
-            var blockTop = parseInt($(this).css('top'));
-            if (blockTop >=0 && blockTop <= maxMove) {
-                var newPageTop = e.pageY;
-                $(this).css('top', '+=' + parseInt(newPageTop - pageTop));
-                pageTop = newPageTop;
+        changeBlockPos: function(curPos, maxMove) {
+            curPos = Math.min(curPos, maxMove);
+            curPos = Math.max(curPos, 0);
+            return curPos;
+        },
+        doScrollAnimation: function($ele, curPos, useAnimate) {
+            if (this.o.dir === 'v') {
+                if (useAnimate) {
+                    $ele.stop(true).animate({top: curPos}, this.o.speed);
+                } else {
+                    $ele.css('top', curPos);
+                }
+            } else {
+                if (useAnimate) {
+                    $ele.stop(true).animate({left: curPos}, this.o.speed);
+                } else {
+                    $ele.css('left', curPos);
+                }
             }
-        });
+        }
+    };
 
-        $(this).on('mouseup',function() {
-            $(this).off('mousemove');
-        });
-    });
-
-    $('body').on('mouseup', function() {
-        $block.off('mousemove');
-    });
+    $.fn[pluginName] = function(o) {
+        new Scroller(this, o);
+        return this;
+    };
 })(jQuery);
