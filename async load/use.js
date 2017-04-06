bbd2f4d39daef8127942535f31dcf16a6f617006
林蛤蛤 <linzb93@qq.com>
2017-04-06 22:37:44 +0800

    update

--- async load/use.js
+++ async load/use.js
@@ -1,56 +1,53 @@
 (function() {
-    var loading = [];
-    var loaded = [];
-    var cbStore = [];
-
-    var header = document.getElementsByTagName('head')[0];
-
-    function each(obj, reverse, callback) {
-        var len;
-        var i = 0;
-        if (arguments.length === 2) {
-            callback = reverse;
-            reverse = null;
-        }
-        len = obj.length;
-        if (!reverse) {
-            for (; i < len; i++) {
-                if (callback.call(obj[i], obj[i], i) === false) {
-                    break;
-                }
-            }
-        } else {
-            for (i = len - 1; i >= 0; i--) {
-                if (callback.call(obj[i], obj[i], i) === false) {
-                    break;
-                }
-            }
-        }
-    }
+    var useStore = [];
 
     var use = function(name, cb) {
-        each(cbStore, function(item, index) {
-            if (item.name === name) {
-                item.cb.push(cb);
-            }
-        });
+        useStore[name].cb.push(cb);
     };
-    use.add = function(config) {
-        var extname = config.path.split('.').pop();
-        if (extname === 'js') {
-           var script = document.createElement('script');
-           script.src = config.path;
-           header.appendChild(script);
-        } else if (extname === 'css') {
-            var style = document.createElement('link');
-            style.setAttribute('rel', 'stylesheet');
-            style.setAttribute('href', config.path);
-            header.appendChild(style);
+
+    use.add = function(name, path, rely) {
+        if (useStore[name]) {
+            throw useStore[name] + ' exist!';
+        }
+        useStore[name] = {};
+        useStore[name].path = path;
+        useStore[name].loaded = false;
+        useStore[name].cb = [];
+        useStore[name].decide = [];
+        if (!rely) {
+            loadScript(name, path);
+        } else {
+            if (useStore[rely].loaded) {
+                loadScript(name, path);
+            } else {
+                useStore[rely].decide.push(name);
+            }
         }
     };
 
-    setInterval(function() {
-        if () {}
-    }, 100);
+    function loadScript(name, path) {
+        var s = document.createElement('script');
+        s.src = path;
+        document.body.appendChild(s);
+        s.onload = s.onreadystatechange = function() {
+            useStore[name].loaded = true;
+            var cbLen = useStore[name].cb.length;
+            if (cbLen) {
+                for (var i = 0; i < cbLen; i++) {
+                    useStore[name].cb[i]();
+                }
+            }
+            if (useStore[name].decide.length) {
+                for (var i = 0, len = useStore[name].decide.length; i < len; i++) {
+                    emitToLoadScript(useStore[name].decide[i]);
+                }
+            }
+        };
+    }
+
+    function emitToLoadScript(name) {
+        loadScript(name, useStore[name].path);
+    }
+
     window.use = use;
 })();
