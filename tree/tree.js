8de7a05bff9dade6580c498da94ba1ee69ec92be
L IN <linzb93@qq.com>
2017-06-23 11:35:37 +0800

    完成树插件的基本render功能

--- tree/tree.js
+++ tree/tree.js
@@ -1,31 +1,40 @@
-(function($, window) {
+(function($) {
     var defaults = {};
 
     function Tree($this, option) {
         this.opt = $.extend({}, defaults, option);
 
         this.$this = $this;
+        this._treeHtml = '';
+        this._path = [0];
 
         this._init();
     }
 
     Tree.prototype = {
-        _init: function() {},
-        _deepRender: function() {
-            var treeHtml = '';
-            if (!this.opt.content) {
-                treeHtml += '';
-                for () {}
-                this.$this.html(treeHtml);
+        _init: function() {
+            this._render(this.opt.items);
+        },
+        _render: function(item) {
+            var i = 0;
+            var len = item.length;
+            while(i < len) {
+                this._treeHtml += '<ul>\
+                                    <li>\
+                                        <a href="#">' + item[i].title + '</a>';
+                if ('childrens' in item[i]) {
+                    this._render(item[i].childrens);
+                }
+                i++;
+                this._treeHtml += '</li></ul>';
             }
         },
-        delete: function(node) {},
-        close: function(node) {},
-        open: function(node) {},
-        toggleCheck: function(node, isCheck) {}
+        html: function() {
+            this.$this.html(this._treeHtml);
+        }
     }
 
     $.fn.tree = function(option) {
-        return new Tree($(this), tree);
+        return new Tree($(this), option).html();
     };
-})(jQuery, window);
+})(jQuery);
