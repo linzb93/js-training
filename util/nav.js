64a4d595460230148f70a4def209d7dbf8bb26f3
L IN <linzb93@users.noreply.github.com>
2017-08-13 22:50:55 +0800

    update

---
+++ util/nav.js
@@ -0,0 +1,36 @@
+var winW = $(window).width();
+var winH = $(window).height();
+var bodyH = $('body').height();
+
+function nav(modules) {
+    var tops = [];
+    var $nav = $('.nav');
+    var $htmlBody = $('html, body');
+    var len = modules.length;
+    $.each(modules, function(index, item) {
+        var top = item.offset().top;
+        if (top > bodyH - winH) {
+            top = bodyH - winH;
+        }
+        tops.push(top);
+    });
+    $(window).on('scroll', util.throttle(function() {
+        var top = $(this).scrollTop();
+        $.each(tops, function(index, item) {
+            if (item > top) {
+                $nav.children('a').removeClass('on').eq(index - 1).addClass('on');
+                return false;
+            }
+            if (index === len - 1) {
+                $nav.children('a').removeClass('on').last().addClass('on');
+            }
+        });
+
+        $nav.on('click', 'a', function() {
+            var index = $(this).index();
+            $htmlBody.animate({
+                scrollTop: tops[index]
+            }, 500);
+        });
+    }));
+}
