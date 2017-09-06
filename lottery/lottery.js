2b68bb5eb20509215b27a90ec84bb3bf74409430
L IN <linzb93@users.noreply.github.com>
2017-09-06 22:02:33 +0800

    添加抽奖插件

---
+++ lottery/lottery.js
@@ -0,0 +1,73 @@
+(function($) {
+    $.fn.lottery = function(option) {
+        var defaults = {};
+
+        var opt = $.extend({}, defaults, option);
+        var ctx = this;
+        var length = this.children().length;
+        var width = this.width();
+        var height = this.height();
+        var cellWidth = this.children().width();
+        var cellHeight = this.children().height();
+        var hMax = Math.floor(width / cellWidth); //水平方向最多能放置的数量
+        var vMax = Math.floor(height / cellHeight); //竖直方向最多能放置的数量
+
+        function initLayout() {
+            var count = calculateEachNum();
+            var hGap = Math.floor((width - cellWidth * count.h) / (count.h - 1));
+            var vGap = Math.floor((height - cellHeight * count.v) / (count.v - 1));
+            this.children().each(function(index) {
+                var left = 0;
+                var top = 0;
+                if (index < length / 2) {
+                    if (index < count.h) {
+                        left = (cellWidth + hGap) * index;
+                        top = 0;
+                    } else {
+                        left = (cellWidth + hGap) * (count.h - 1);
+                        top = (cellHeight + vGap) * (index - count.h);
+                    }
+                } else {
+                    if (index < (count.h * 2 + count.v)) {
+                        left = (cellWidth + hGap) * (length - index - count.v);
+                        top = (cellHeight + vGap) * (count.v - 1);
+                    } else {
+                        left = 0;
+                        top = (cellHeight + vGap) * (length - index);
+                    }
+                }
+                $(this).css({
+                    left: left,
+                    top: top
+                });
+            })
+        }
+
+        function calculateEachNum() {
+            var hCount = hMax;
+            var vCount = (length - hCount) / 2 + 2;
+
+            var hGap = Math.floor((width - cellWidth * hCount) / (hCount - 1));
+            var vGap = Math.floor((height - cellHeight * vCount) / (vCount - 1));
+            while(hGap < vGap) {
+                hCount -= 1;
+                vCount += 1;
+                hGap = Math.floor((width - cellWidth * hCount) / (hCount - 1));
+                vGap = Math.floor((height - cellHeight * vCount) / (vCount - 1));
+            }
+            return {
+                h: hCount,
+                v: vCount
+            };
+        }
+
+        function lotteryBegin() {}
+
+        return  {
+            begin: function() {
+                lotteryBegin();
+            }
+        }
+
+    }
+})(jQuery);
